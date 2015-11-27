38528d32496a047460cc68e626216e8a1cf74ee2
Fernando Rodriguez <frodriguez.developer@outlook.com>
2015-11-27 14:35:47 -0500

    Added a few more icons

--- icons/mono-plus/make.sh
+++ icons/mono-plus/make.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo 'Creating icon symlinks...'
-ln -s java-icon48.svg scalable/apps/sun-jcontrol-oracle-jdk-bin-1.8.svg
-ln -s kappfinder.svgz scalable/apps/baloo.svgz
+ln -sv java-icon48.svg scalable/apps/sun-jcontrol-oracle-jdk-bin-1.8.svg
+ln -sv kappfinder.svgz scalable/apps/baloo.svgz
+ln -sv akonadi.svgz scalable/apps/akonaditray.svgz
