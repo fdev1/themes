8b7ee5d795628e7b461dbad5a97e11d989b1887f
Fernando Rodriguez <frodriguez.developer@outlook.com>
2015-12-04 19:22:04 -0500

    Added a few more icons

--- icons/mono-plus/make.sh
+++ icons/mono-plus/make.sh
@@ -6,3 +6,11 @@ ln -sv kappfinder.svgz scalable/apps/baloo.svgz
 ln -sv akonadi.svgz scalable/apps/akonaditray.svgz
 ln -sv akonadi.svgz scalable/apps/akonadiconsole.svgz
 ln -sv bug.svg scalable/apps/kdbg.svg
+ln -sv application-x-tar.svgz scalable/mimetypes/application-x-tarz.svgz
+ln -sv application-x-tar.svgz scalable/mimetypes/application-x-tzo.svgz
+ln -sv application-x-tar.svgz scalable/mimetypes/application-x-xz-compressed-tar.svgz
+ln -sv application-x-tar.svgz scalable/mimetypes/application-x-lzma-compressed-tar.svgz
+ln -sv application-x-tar.svgz scalable/mimetypes/application-x-lrzip-compressed-tar.svgz
+ln -sv application-x-tar.svgz scalable/mimetypes/application-x-bzip-compressed-tar.svgz
+ln -sv application-x-tar.svgz scalable/mimetypes/application-x-compressed-tar.svgz
+ln -sv application-x-tar.svgz scalable/mimetypes/application-x-ustar.svgz
